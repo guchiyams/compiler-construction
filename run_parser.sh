c560edc18f9155a271093c98a6eb81e00758739d
guchiyam <yamatoe1227@gmail.com>
2023-05-14 18:43:24 -0700

    README

--- run_parser.sh
+++ run_parser.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-clang++ -std=c++17 main.cpp -o parse
-sudo chmod u+x parse
+clang++ -std=c++17 main.cpp -o compile
+sudo chmod u+x compile
 
 $SCRIPT_DIR/parse $1
--- submission/run_parser.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-clang++ -std=c++17 main.cpp -o parse
-sudo chmod u+x parse
-
-$SCRIPT_DIR/parse $1
